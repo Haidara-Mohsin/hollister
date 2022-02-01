1aea1ad74c9fa1489511030f37569b8895c0354f
Haidara <ha00004@mix.wvu.edu>
2022-02-01 18:27:39 -0500

    Assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { SecondNavBarComponent } from 'src/navigation/secondnavbar.component';
-import { TopNavBarComponent } from 'src/navigation/topnavbar.component';
+import { SecondNavBarComponent } from 'src/app/navigation/secondnavbar.component';
+import { TopNavBarComponent } from 'src/app/navigation/topnavbar.component';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './navigation/footer/footer.component';
+import { SecondFooterComponent } from './navigation/footer/second.footer.component';
+import { CardComponent } from './Store/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
-    SecondNavBarComponent
+    SecondNavBarComponent,
+    FooterComponent,
+    SecondFooterComponent,
+    CardComponent
+
   ],
   imports: [
     BrowserModule
---
+++ src/app/navigation/footer/second.footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'hollister-second-footer',
+    templateUrl: 'second.footer.component.html'
+})
+export class SecondFooterComponent {
+
+}
